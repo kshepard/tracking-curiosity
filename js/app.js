04f3dd915dd845a966744cfbd3c8faa1be43d537
Kenny Shepard <kshepard@gmail.com>
2013-04-21 10:38:23 -0400

    Hook up map and sidebar

--- js/app.js
+++ js/app.js
@@ -1,21 +1,13 @@
 $(function(){
-	$('.scroll-pane section').waypoint(function(direction) {
-		var $active = $(this);
-  		$active.toggleClass('active');
-	}, {
-  		offset: '33.33%'
-  	});
-
-    // listen for 'drive-change' events
-    $(document).bind('drive-change', function (e, drive, data, lon, lat) {
-        console.log('drive-change received in app.js: ', drive);
-    });
-
     // build drive sections
     $.getJSON('data/drive.json', function (data) {
+        var drives = {};
+        
         $.each(data.features, function (i, feature) {
+            drives[feature.properties.drive] = feature;
+
             var section = '' +
-		'<section>' +
+		'<section data-drive-id="' + feature.properties.drive + '">' +
 		'	<h3>Drive ' + feature.properties.drive + '</h3>' +
 		'	<div class="distance-traveled">Distance Traveled: <span>' + feature.properties.dist + '</span></div>' +
 		'	<div class="duration">Duration: <span>' + feature.properties.duration+ '</span></div>' +
@@ -26,6 +18,24 @@ $(function(){
 		'</section>';
             
             $(section).appendTo($('#section-container'));
+        });
+
+        // initialize waypoints
+	$('.scroll-pane section').waypoint(function(direction) {
+	    var $active = $(this);
+  	    $active.toggleClass('active');
+
+            var driveId = $active.data('drive-id');
+            if (driveId !== undefined) {
+                var feature = drives[driveId];
+                $(document).trigger('drive-change', [driveId, feature.properties, feature.geometry.coordinates[0], feature.geometry.coordinates[1]]);
+            }
+	}, {
+  	    offset: '33.33%'
+  	});
+
+        // listen for 'drive-change' events
+        $(document).bind('drive-change', function (e, drive, data, lon, lat) {
         });
     });
 });
